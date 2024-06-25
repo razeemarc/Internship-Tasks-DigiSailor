a548c0d2803ec0f42fed6e962844c74cd687e2fd
Razeema R C <122087518+razeemarc@users.noreply.github.com>
2024-06-25 07:17:06 +0530

    Delete Task1 - CRUD Operations directory

--- Task1 - CRUD Operations/project1/src/main/java/com/javaguides/project1/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javaguides.project1.repository;
-
-import com.javaguides.project1.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
