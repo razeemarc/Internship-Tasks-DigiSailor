5cb02b3c82193e369136e9e4f69323ff3ccd2681
Razeema R C <122087518+razeemarc@users.noreply.github.com>
2024-07-03 11:34:55 +0530

    Delete Task-5 Spring State Machine directory

--- Task-5 Spring State Machine/src/main/java/com/example/demo/OrderEvents.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
-
-
-public enum OrderEvents {
-    PAY, SHIP, DELIVER
-}
--- Task-5 Spring State Machine/src/main/java/com/example/demo/OrderStates.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
-
-
-public enum OrderStates {
-    ORDERED, PAID, SHIPPED, DELIVERED
-}
